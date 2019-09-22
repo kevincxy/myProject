58b98e28502ce57a565ce75a240a465ca14d2bda
kevincxy <changxiangyang1@newhope.cn>
2019-09-22 21:06:13 +0800

    学习父子组件传值

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -6,12 +6,33 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 @Input("title") title:string;
+@Input("check") check:any;
+@Input("page") page:any;
+
+public msg:string="我是子组件的msg";
+//声明一个事件属性
+@Output("outer") public outer:any = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
 
   }
+  getTitle(){
+    console.log(this.title);
+  }
   run(){
-    console.log("我是header的run方法");
+   alert("我是header的run方法");
+  }
+  //执行父组件的方法
+  executeParentMethod(){
+    //执行父组件的check方法
+    // this.check();
+    this.page.check();
+  
+  }
+  //给父组件发广播
+  sendParent(){
+    this.outer.emit("你是个大傻逼，哈哈哈~~~");
   }
 }
--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-page',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComponent implements OnInit {
 public title:string= "首页组件title";
+//获取子组件的实体
+@ViewChild("header",{static:true}) header:any;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  check(e){
+    alert(e);
+    alert("我是父组件里的check方法！");
+    
+  }
+  //获取子组件的msg值
+  getChildMsg(){
+    console.log(this.header.msg);
+  }
+  //执行子组件方法
+  execChildRun(){
+    this.header.run();
+  }
+
 
 }
